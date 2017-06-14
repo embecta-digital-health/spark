e61dde56c4deb8ba137416fa5233b99249e8adbf
DESKTOP-PKE0TSR\David Downey <david_downey@bd.com>
2017-06-14 15:27:35 -0400

    first commit

---
+++ src/Spark.Engine.Core/Model/FhirResourceBinder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Hl7.Fhir.Model;
+using Microsoft.AspNetCore.Mvc.Internal;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.CSharp.RuntimeBinder;
+using Spark.Core;
+using Spark.Engine.Core;
+using Spark.Engine.Formatters;
+
+// ReSharper disable ArrangeTypeModifiers
+
+namespace Spark.Engine.Model
+{
+    class FhirResourceBinder : IModelBinder
+    {
+        private readonly IFhirStore _db;
+        private JsonFhirInputFormatter _fhirInputFormatter;
+
+        public FhirResourceBinder(IFhirStore db)
+        {
+            _db = db;
+        }
+
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelType);
+            var valueProviderResult2 = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            if (valueProviderResult.Length > 0)
+            {
+                var valueAsString = valueProviderResult.FirstValue;
+
+                if (string.IsNullOrEmpty(valueAsString))
+                {
+                    throw new RuntimeBinderException();
+//                    return _fallbackBinder.BindModelAsync(bindingContext);
+                }
+
+//                _fhirInputFormatter.ReadRequestBodyAsync();
+
+//                var result = HtmlEncoder.Default.Encode(valueAsString);
+                var result = new Practitioner(){Id = "newprac000001", Active = true};
+                bindingContext.Result = ModelBindingResult.Success(result);
+            }
+
+            return TaskCache.CompletedTask;
+        }
+    }
+}
---
+++ src/Spark.Engine.Core/Service/Transfer.cs
@@ -0,0 +1,51 @@
+﻿using Spark.Core;
+using System.Collections.Generic;
+using Spark.Engine.Core;
+
+namespace Spark.Service
+{
+
+    /// <summary>
+    /// Transfer maps between local id's and references and absolute id's and references upon incoming or outgoing Interactions.
+    /// It uses an Import or Export to do de actual work for incoming or outgoing Interactions respectively.
+    /// </summary>
+    public class Transfer
+    {
+        ILocalhost localhost;
+        IGenerator generator;
+
+        public Transfer(IGenerator generator, ILocalhost localhost)
+        {
+            this.generator = generator;
+            this.localhost = localhost;
+        }
+
+        public void Internalize(Entry entry)
+        {
+            var import = new Import(this.localhost, this.generator);
+            import.Add(entry);
+            import.Internalize();
+        }
+
+        public void Internalize(IEnumerable<Entry> interactions)
+        {
+            var import = new Import(this.localhost, this.generator);
+            import.Add(interactions);
+            import.Internalize();
+        }
+
+        public void Externalize(Entry interaction)
+        {
+            Export export = new Export(this.localhost);
+            export.Add(interaction);
+            export.Externalize();
+        }
+
+        public void Externalize(IEnumerable<Entry> interactions)
+        {
+            Export export = new Export(this.localhost);
+            export.Add(interactions);
+            export.Externalize();
+        }
+    }
+}
