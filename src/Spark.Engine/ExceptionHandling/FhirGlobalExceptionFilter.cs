82ccd39c50ac8b496c2bb43c864b989b37cab023
DESKTOP-PKE0TSR\David Downey <david_downey@bd.com>
2018-01-03 13:25:03 -0500

    logging

--- src/Spark.Engine/ExceptionHandling/FhirGlobalExceptionFilter.cs
+++ src/Spark.Engine/ExceptionHandling/FhirGlobalExceptionFilter.cs
@@ -1,37 +1,29 @@
-﻿/* 
- * Copyright (c) 2014, Furore (info@furore.com) and contributors
- * See the file CONTRIBUTORS for details.
- * 
- * This file is licensed under the BSD 3-Clause license
- * available at https://raw.github.com/furore-fhir/spark/master/LICENSE
- */
-
+﻿#region
 
 using System;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Mindscape.Raygun4Net;
-using ExceptionFilterAttribute = Microsoft.AspNetCore.Mvc.Filters.ExceptionFilterAttribute;
+
+#endregion
 
 namespace Spark.Engine.ExceptionHandling
 {
     public class FhirGlobalExceptionFilter : ExceptionFilterAttribute
     {
         private static IExceptionResponseMessageFactory _exceptionResponseMessageFactory;
-        private readonly IHostingEnvironment _hostingEnvironment;
-        private readonly IModelMetadataProvider _modelMetadataProvider;
-        private readonly ILogger<FhirGlobalExceptionFilter> _logger;
         private readonly IRaygunAspNetCoreClientProvider _clientProvider;
+        private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly ILogger<FhirGlobalExceptionFilter> _logger;
+        private readonly IModelMetadataProvider _modelMetadataProvider;
         private readonly IOptions<RaygunSettings> _settings;
 
-        public FhirGlobalExceptionFilter(ILogger<FhirGlobalExceptionFilter> logger, IRaygunAspNetCoreClientProvider clientProvider, IOptions<RaygunSettings> raygunSettings)//, IHostingEnvironment hostingEnvironment, IModelMetadataProvider modelMetadataProvider)
+        public FhirGlobalExceptionFilter(ILogger<FhirGlobalExceptionFilter> logger, IRaygunAspNetCoreClientProvider clientProvider, IOptions<RaygunSettings> raygunSettings) //, IHostingEnvironment hostingEnvironment, IModelMetadataProvider modelMetadataProvider)
         {
             _logger = logger;
             _exceptionResponseMessageFactory = new ExceptionResponseMessageFactory();
@@ -41,33 +33,37 @@ namespace Spark.Engine.ExceptionHandling
 //            _modelMetadataProvider = modelMetadataProvider;
         }
 
+        /// <inheritdoc />
         /// <summary>
-        /// https://blog.kloud.com.au/2016/03/23/aspnet-core-tips-and-tricks-global-exception-handling/
+        ///     https://blog.kloud.com.au/2016/03/23/aspnet-core-tips-and-tricks-global-exception-handling/
         /// </summary>
         /// <param name="exceptionContext"></param>
         public override void OnException(ExceptionContext exceptionContext)
         {
-
-            _logger.LogError("++++++From {a}: {b} ---- stacktrace: {c}", this.GetType().Name, exceptionContext.Exception.Message, exceptionContext.Exception.Demystify());
             IActionResult actionResult = _exceptionResponseMessageFactory.GetResponseOutcome(exceptionContext.Exception);
             //exceptionContext.HttpContext.Request.EnableRewind();
             RaygunClient raygunClient = _clientProvider.GetClient(_settings.Value, exceptionContext.HttpContext);
-            
+            int? statusCode = StatusCode(actionResult);
+            string user = exceptionContext.HttpContext.User.Identity.Name;
             raygunClient.SendInBackground(exceptionContext.Exception);
-            exceptionContext.Result = (actionResult);
+            _logger.LogError("{{ 'who': '{who}', 'when': '{when}', 'what': '{what}', 'detail': ' statusCode {statusCode}: class: {class}  message: {message} ---- stacktrace: {stacktrace} '}}",
+                user, DateTime.Now, "exception", statusCode, GetType().Name, exceptionContext.Exception.Message, exceptionContext.Exception.Demystify());
+            exceptionContext.Result = actionResult;
+        }
 
+        private static int? StatusCode(IActionResult actionResult)
+        {
+            int? statusCode = null;
+            try
+            {
+                statusCode = ((ObjectResult) actionResult).StatusCode;
+            }
+            catch (InvalidCastException e)
+            {
+                Console.WriteLine(e);
+            }
 
-
-            //            if (!_hostingEnvironment.IsDevelopment())
-//            {
-//                 do nothing
-//                return;
-//            }
-//            var result2 = new ViewResult { ViewName = "CustomError" };
-//            result2.ViewData = new ViewDataDictionary(_modelMetadataProvider, exceptionContext.ModelState);
-//            result2.ViewData.Add("Exception", exceptionContext.Exception);
-//             TODO: Pass additional detailed data via ViewData
-//            exceptionContext.Result = result2;
+            return statusCode;
         }
     }
 }
